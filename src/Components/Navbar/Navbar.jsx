6ee4a0e0d9ad01a4a00d92ec915c2db7d9f00a38
KorlapuDev <korlapu9@gmail.com>
2024-12-03 22:46:14 +0530

    navbar changes and main layout outlet setup

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,52 +1,7 @@
-import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
-import "./Navbar.css";
+import React from "react";
 
 const Navbar = () => {
-  return (
-    <header className="header">
-      <nav className="nav container">
-        <NavLink to="/" className="nav__logo">
-          Navigation Bar
-        </NavLink>
-
-        <div className={"nav__menu"} id="nav-menu">
-          <ul className="nav__list">
-            <li className="nav__item">
-              <NavLink to="/" className="nav__link">
-                Home
-              </NavLink>
-            </li>
-            <li className="nav__item">
-              <NavLink to="/news" className="nav__link">
-                News
-              </NavLink>
-            </li>
-            <li className="nav__item">
-              <NavLink to="/about-us" className="nav__link">
-                About Us
-              </NavLink>
-            </li>
-            <li className="nav__item">
-              <NavLink to="/favorite" className="nav__link">
-                Favorite
-              </NavLink>
-            </li>
-            <li className="nav__item">
-              <NavLink to="/location" className="nav__link">
-                Location
-              </NavLink>
-            </li>
-            <li className="nav__item">
-              <NavLink to="/get-started" className="nav__link nav__cta">
-                Get Started
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </header>
-  );
+  return <div>Navbar</div>;
 };
 
 export default Navbar;
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,8 +1,17 @@
 import { Box } from "@mui/material";
 import React from "react";
+import { Outlet } from "react-router-dom"; // Import Outlet
+import Navbar from "../Components/Navbar/Navbar";
 
 const MainLayout = ({ children }) => {
-  return <Box width="100%">{children}</Box>;
+  return (
+    <Box width="100%">
+      {children}
+      <Outlet />
+      {/* Render nested routes here */}
+      <Navbar />
+    </Box>
+  );
 };
 
 export default MainLayout;
